d28e58f6a061a00e0738075250b1a6cb14a19eda
thisisaaronland <devnull@localhost>
2020-10-09 15:29:36 -0700

    add an index/usage page

--- http/lookup.go
+++ http/lookup.go
@@ -23,6 +23,18 @@ func LookupHandler(r reader.Reader) (go_http.Handler, error) {
 		ctx := req.Context()
 		path := req.URL.Path
 
+		if path == "/" {
+
+			err := usage(rsp, req)
+
+			if err != nil {
+				go_http.Error(rsp, err.Error(), go_http.StatusInternalServerError)
+				return
+			}
+
+			return
+		}
+
 		id, _, err := uri.ParseURI(path)
 
 		if err != nil {
@@ -80,4 +92,31 @@ func LookupHandler(r reader.Reader) (go_http.Handler, error) {
 	}
 
 	return go_http.HandlerFunc(fn), nil
+}
+
+func usage(rsp go_http.ResponseWriter, req *go_http.Request) error {
+
+	rsp.Header().Set("Content-type", "text/html")
+
+	rsp.Write([]byte(`<!DOCTYPE html>
+<html>
+ <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <title>Who's On First Finding Aid</title>
+ </head>
+ <body>
+ <h1>Who's On First Finding Aid</h1>
+ <p>For example:</p>
+ <pre>
+$> curl -s <a href="https://data.whosonfirst.org/findingaid/85633041">https://data.whosonfirst.org/findingaid/85633041</a> | jq
+{
+  "id": 85633041,
+  "uri": "856/330/41/85633041.geojson",
+  "repo": "whosonfirst-data-admin-ca"
+}
+ </pre>
+ <p style="font-style:italic;">If you're reading this there may still be some missing data that is in the process of being updated.</p>
+</body>`))
+
+	return nil
 }
