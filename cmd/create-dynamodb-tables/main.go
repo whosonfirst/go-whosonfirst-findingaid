983be586aafd95ca0d46ff0572770d689a957898
thisisaaronland <aaron@localhost>
2021-11-04 11:23:46 -0700

    snapshot: update to use -dynamodb-uri w/ local dynamodb instance

--- cmd/create-dynamodb-tables/main.go
+++ cmd/create-dynamodb-tables/main.go
@@ -1,32 +1,72 @@
-package doctore
+package main
 
 import (
 	"flag"
 	"github.com/aaronland/go-aws-dynamodb"
 	"github.com/aaronland/go-aws-session"
 	"github.com/aws/aws-sdk-go/aws"
+	aws_session "github.com/aws/aws-sdk-go/aws/session"
 	aws_dynamodb "github.com/aws/aws-sdk-go/service/dynamodb"
 	"log"
+	"net/url"
 )
+
+/*
+
+> go run -mod vendor cmd/create-dynamodb-tables/main.go -dynamodb-uri 'awsdynamodb://findinaid?region=us-west-2&endpoint=http://localhost:8000&credentials=static:local:local:local'
+2021/11/04 11:23:03 Failed to create tables, Failed to create table 'findinaid', InvalidParameter: 1 validation error(s) found.
+- missing required field, CreateTableInput.KeySchema.
+exit status 1
+
+*/
 
 func main() {
 
-	aws_dsn := flag.String("aws-dsn", "", "A valid aaronland/go-aws-session DSN")
-	table_name := flag.String("table-name", "findingaid", "...")
+	dynamodb_uri := flag.String("dynamodb-uri", "", "A valid aaronland/go-aws-session DSN")
 	billing_mode := flag.String("billing-mode", "PAY_PER_REQUEST", "...")
 
 	flag.Parse()
 
-	sess, err := session.NewSessionWithDSN(*aws_dsn)
+	// START OF put me in aaronland/go-aws-dynamodb
+
+	u, err := url.Parse(*dynamodb_uri)
 
 	if err != nil {
-		log.Fatalf("Failed to create AWS session, %v", err)
+		log.Fatalf("Failed to parse URI, %v", err)
+	}
+
+	table_name := u.Host
+
+	q := u.Query()
+
+	// partition_key := q.Get("partition_key")
+	region := q.Get("region")
+	endpoint := q.Get("endpoint")
+
+	credentials := q.Get("credentials")
+
+	cfg, err := session.NewConfigWithCredentialsAndRegion(credentials, region)
+
+	if err != nil {
+		log.Fatalf("Failed to create new session for credentials '%s', %w", credentials, err)
+	}
+
+	if endpoint != "" {
+		cfg.Endpoint = aws.String(endpoint)
+	}
+
+	sess, err := aws_session.NewSession(cfg)
+
+	if err != nil {
+		log.Fatalf("Failed to create AWS session, %w", err)
 	}
 
 	client := aws_dynamodb.New(sess)
 
+	// END OF put me in aaronland/go-aws-dynamodb
+
 	tables := map[string]*aws_dynamodb.CreateTableInput{
-		*table_name: &aws_dynamodb.CreateTableInput{
+		table_name: &aws_dynamodb.CreateTableInput{
 			AttributeDefinitions: []*aws_dynamodb.AttributeDefinition{
 				{
 					AttributeName: aws.String("id"),
@@ -61,7 +101,7 @@ func main() {
 				},
 			*/
 			BillingMode: aws.String(*billing_mode),
-			TableName:   aws.String(*table_name),
+			TableName:   aws.String(table_name),
 		},
 	}
 
