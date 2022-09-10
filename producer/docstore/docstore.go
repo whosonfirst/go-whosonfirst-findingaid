a96aee5529806aa27e4399c436a7e425a4c9a598
thisisaaronland <devnull@localhost>
2022-09-09 17:07:52 -0700

    update producer/docstore/docstore.go to check existing value and skip if it is the same as a the (new) value to set

--- producer/docstore/docstore.go
+++ producer/docstore/docstore.go
@@ -14,12 +14,27 @@ type CatalogRecord struct {
 
 func AddToCatalog(ctx context.Context, collection *gc_docstore.Collection, id int64, repo_name string) error {
 
+	test_doc := map[string]interface{}{
+		"id":        id,
+		"repo_name": "",
+	}
+
+	err := collection.Get(ctx, test_doc)
+
+	if err != nil {
+		return fmt.Errorf("Failed to get record for %d, %w", id, err)
+	}
+
+	if test_doc["repo_name"] == repo_name {
+		return nil
+	}
+
 	doc := &CatalogRecord{
 		Id:       id,
 		RepoName: repo_name,
 	}
 
-	err := collection.Put(ctx, doc)
+	err = collection.Put(ctx, doc)
 
 	if err != nil {
 		return fmt.Errorf("Failed to put catalog record for %d, %w", id, err)
